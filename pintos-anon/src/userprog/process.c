96177f9a755b181374bb58ad2b041a0f0b6872f2
NightL-Sorrow <linzhy@shanghaitech.edu.cn>
2019-10-26 19:32:15 +0800

    Trial fix of argument passing

--- pintos-anon/src/userprog/process.c
+++ pintos-anon/src/userprog/process.c
@@ -20,6 +20,66 @@
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
+bool argument_pass(char *save_ptr, void **esp){
+  /* Setup stack success, allocate arguments for process 
+    Max 256 arguments saved in argv Left-To-Right */
+  const int arg_limit = 256;
+  char *argv[arg_limit];
+
+  /* Stack Pointer calculate in bytes */
+  char *sp = (char *) esp;  
+  int argc = 0;
+  int size = 4 + 4 + 4 + 4;
+  char *token = NULL;
+
+  /* Already skip the file name;
+    Impose a 4kB limit on arguments passing */
+  for (token = strtok_r (NULL, " ", &save_ptr); token != NULL;
+      token = strtok_r (NULL, " ", &save_ptr)){
+
+        size += strlen(token)+1  + 4;
+
+        // if(argc+1 == arg_limit || size > PGSIZE){
+        //   printf("Passing arguments over %d numbers or length over 4kB\n",
+        //         arg_limit);
+        //  return false;
+        // }
+
+        sp -= strlen(token)+1;
+        strlcpy(sp, token, strlen(token)+1);
+        argv[argc++] = sp;
+      }
+
+  /* align; no guarante what inside this gap */
+  while((int)sp % 4 != 0) sp--;
+
+  /* Very Important:
+    we need cast in the rest to make it insert in 4bytes */
+  /* push a zero */
+  sp -= 4;
+  *(int *)sp = 0;
+
+  /* push arg pointer Right-To-Left */
+  for (int i=argc-1; i>=0; i--){
+    sp -= 4;
+    *(char **)sp = argv[i];
+  }
+
+  /* push arg pointer */
+  sp -= 4;
+  *(char **)sp = sp+4;
+    
+  /* push arg counter */
+  sp -= 4;
+  *(int *)sp = argc;
+
+  /* push return address */
+  sp -= 4;
+  *(int *)sp = 0;
+
+  esp = sp;
+  return true;
+}
 
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
@@ -63,7 +123,15 @@ start_process (void *file_name_)
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp);
+
+  /* slpit file_name from cmd */
+  char *save_ptr = NULL;
+  char *thread_name = strtok_r(file_name," ", &save_ptr);
+  success = load (thread_name, &if_.eip, &if_.esp);
+
+  if(success){
+    argument_pass(save_ptr,if_.esp);
+  }
 
   /* If load failed, quit. */
   palloc_free_page (file_name);
@@ -92,6 +160,7 @@ start_process (void *file_name_)
 int
 process_wait (tid_t child_tid UNUSED) 
 {
+  timer_sleep(10000000);
   return -1;
 }
 
@@ -213,7 +282,7 @@ static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
    and its initial stack pointer into *ESP.
    Returns true if successful, false otherwise. */
 bool
-load (const char *input_cmd, void (**eip) (void), void **esp) 
+load (const char *file_name, void (**eip) (void), void **esp) 
 {
   struct thread *t = thread_current ();
   struct Elf32_Ehdr ehdr;
@@ -221,16 +290,6 @@ load (const char *input_cmd, void (**eip) (void), void **esp)
   off_t file_ofs;
   bool success = false;
   int i;
-
-  /* ==================== project 2 =================== */
-  /* Setup stack success, allocate arguments for process 
-    Max 256 arguments saved in argv Left-To-Right */
-  const int arg_limit = 256;
-  char *argv[arg_limit];
-
-  /* slpit file_name from cmd */
-  char *save_ptr = NULL;
-  char *file_name = strtok_r(input_cmd," ", &save_ptr);
 
   /* Allocate and activate page directory. */
   t->pagedir = pagedir_create ();
@@ -321,61 +380,6 @@ load (const char *input_cmd, void (**eip) (void), void **esp)
   /* Set up stack. */
   if (!setup_stack (esp))
     goto done;
-
-/* ============================ project 2 ============================= */
-
-  /* Stack Pointer calculate in bytes */
-  char *sp = (char *) esp;
-  
-  int argc = 0;
-  int size = 4 + 4 + 4 + 4;
-  char *token = NULL;
-
-  /* Already skip the file name;
-    Impose a 4kB limit on arguments passing */
-  for (token = strtok_r (input_cmd, " ", &save_ptr); token != NULL;
-      token = strtok_r (NULL, " ", &save_ptr)){
-
-        size += strlen(token)+1  + 4;
-
-        if(argc+1 == arg_limit || size > PGSIZE){
-          printf("Passing arguments over %d numbers or length over 4kB\n",
-                arg_limit);
-          goto done;
-        }
-
-        sp -= strlen(token)+1;
-        strlcpy(sp, token, strlen(token)+1);
-        argv[argc++] = sp;
-      }
-
-  /* align; no guarante what inside this gap */
-  while((int)sp % 4 != 0) sp--;
-
-  /* push a zero */
-  sp -= 4;
-  *sp = 0;
-
-  /* push arg pointer Right-To-Left */
-  for (int i=argc-1; i>=0; i--){
-    sp -= 4;
-    *sp = argv[i];
-  }
-
-  /* push arg pointer */
-  sp -= 4;
-  *sp = sp+4;
-  
-  /* push arg counter */
-  sp -= 4;
-  *sp = argc;
-
-  /* push return address */
-  sp -= 4;
-  *sp = 0;
-
-  esp = sp;
-/* ================================================== */
 
   /* Start address. */
   *eip = (void (*) (void)) ehdr.e_entry;
