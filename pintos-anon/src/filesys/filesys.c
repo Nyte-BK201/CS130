ef9b7c74b6c430d32292571f5714ac13c630d400
miniwmy <mini.wmy@hotmail.com>
2019-12-10 18:34:44 +0800

    write back to disk when filesys_done()

--- pintos-anon/src/filesys/filesys.c
+++ pintos-anon/src/filesys/filesys.c
@@ -24,7 +24,7 @@ filesys_init (bool format)
 
   inode_init ();
   free_map_init ();
-  cache_init();
+  cache_init ();
 
   if (format) 
     do_format ();
@@ -38,6 +38,7 @@ void
 filesys_done (void) 
 {
   free_map_close ();
+  cache_clear ();
 }
 
 /* Creates a file named NAME with the given INITIAL_SIZE.
